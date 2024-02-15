55df4cc1e7a924e7d5c37a0b96404b9bd76f1a75
Stim Fry Mac <timothy.atoyo@gmail.com>
2024-02-15 17:21:16 +0000

    Adds controlled search input, implement resuts to search results card

--- src/components/SearchResultContainer.jsx
+++ src/components/SearchResultContainer.jsx
@@ -1,21 +1,51 @@
 import React, { useState } from 'react'
 import SingleCardResult from './SingleCardResult'
 import SearchForm from './SearchForm'
-import Api from './utils/Api'
+import Api from '../utils/Api'
+
 function SearchResultContainer() {
   const [searchData, setSearchData] = useState({
     search: '',
-    result: []
+    results: []
   })
 
-  const searchMovie = (queryString) => {
- Api.search(queryString)
-    .then(res => console.log(res))
+  const searchMovie = ( queryString ,isAdultContent) => {
+ Api.search(queryString, isAdultContent)
+    .then(res => {
+      // console.log(res.data.results[0]);
+      setSearchData({ 
+      ...searchData, 
+      results: res.data.results[0] })})
   }
+
+  // searchMovie('matrix', 'true')
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+ console.log(event.target);
+ console.log([name]);
+
+    setSearchData({
+      ...searchData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    searchMovie(searchData.search);
+  };
+
   return (
     <div>
-      <SearchForm  />
-      <SingleCardResult />
+      <SearchForm  
+            search={searchData.search}
+            handleFormSubmit={handleFormSubmit}
+            handleInputChange={handleInputChange}
+      />
+      <SingleCardResult 
+      results={searchData.results}
+      />
     </div>
   )
 }
--- src/components/SingleCardResult.jsx
+++ src/components/SingleCardResult.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
-function SingleCardResult(props) {
+function SingleCardResult({results}) {
   return (
     <div>
-      <div className="m-4 mb-8 w-auto px-4 mx-auto sm:w-1/2 md:w-1/3 lg:w-1/4">
+  <div className="flex m-4 mb-8 w-auto px-4 mx-auto sm:w-1/2 ">
+    
   <div className="rounded-lg bg-white shadow-lg">
-    <img src="https://source.unsplash.com/400x500/?dark" alt="movie poster" className="rounded-t-lg" />
     <div className="p-4">
-      <h2 className="mb-2 text-lg font-semibold">Movie Title</h2>
-      <p className="mb-2 text-sm text-gray-700">Release Date: January 1, 2023</p>
+    <img src={`https://image.tmdb.org/t/p/original/${results.poster_path}`} alt="movie poster" className="rounded-t-lg" />
+    </div>
+  </div>
+  <div className="rounded-lg bg-white shadow-lg">
+    <div className="p-4">
+      <h1 className="mb-2 text-lg font-semibold">{results.original_title}</h1>
+      <p className="mb-2 text-sm text-gray-700">Release Date: {results.release_date}</p>
       <p className="mb-4 text-sm text-gray-700">Director: John Doe</p>
+      <p className="mb-4 text-sm text-gray-700">{results.overview}</p>
       <a href="#" className="block rounded-lg bg-blue-500 px-4 py-2 text-center font-semibold text-white hover:bg-blue-600">Watch Movie</a>
     </div>
   </div>
