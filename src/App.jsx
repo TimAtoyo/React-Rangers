1ef0732e45c3e5ea7203ed585adfd54136b526ff
ptak18 <ptakyie@gmail.com>
2024-02-15 19:56:11 +0000

    refactoing the nav component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ const router = createBrowserRouter([
   {
     element: <Navbar />, 
     children: [
-      {path: '/', 
+      {
+        path: '/', 
       element:  <SearchResultContainer />,
     }
     // ,
