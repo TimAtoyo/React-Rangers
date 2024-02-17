821b3c80abcc2322a7b2b13f2c233e4576201b33
sonia-robin <cervinkova.sonik@gmail.com>
2024-02-17 23:32:09 +0000

    Adds component to fetch data from API and display movies by genre

---
+++ src/components/MoviesByGenres.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import GetMovies from "../utils/Api-movies-filters";
+import Genres from "./Genres";
+
+function MoviesByGenres() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await GetMovies();
+        console.log(data);
+        setResults(data.results); // Update the state with the fetched genres
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle error
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Genres />
+      <div className="flex justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+        {results &&
+          results.map((result) => (
+            <div className="max-w-xs rounded overflow-hidden shadow-lg">
+              <img
+                className="w-full"
+                src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
+                alt="poster"
+              ></img>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{result.title}</div>
+                <p className="text-gray-700 text-base">{result.release_date}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default MoviesByGenres;
