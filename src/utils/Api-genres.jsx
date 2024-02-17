5167a2f99e58f1fe99327da173b2423b6e098b26
sonia-robin <cervinkova.sonik@gmail.com>
2024-02-17 23:29:24 +0000

    Renames Api-filters to APi-genres

---
+++ src/utils/Api-genres.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const apiKey = 'abc3ed8a831072a48d6f1c34def28099';
+
+const GetGenres = async () => {
+    try {
+        const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list', {
+            params: {
+                language: 'en',
+                api_key: apiKey
+            },
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        console.log(response.data);
+        console.log(response.data.genres[0].name);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // Re-throw the error if needed
+    }
+};
+
+export default GetGenres
