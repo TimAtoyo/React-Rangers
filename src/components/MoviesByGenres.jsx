7fbe5b615473eeb85f74e22a997613b0dc31aa27
sonia-robin <cervinkova.sonik@gmail.com>
2024-02-18 17:47:27 +0000

    Passes genresString prop and adds key to movie cards

--- src/components/MoviesByGenres.jsx
+++ src/components/MoviesByGenres.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import GetMovies from "../utils/Api-movies-filters";
-import Genres from "./Genres";
+// import Genres from "./Genres";
 
-function MoviesByGenres() {
+function MoviesByGenres({ genresString }) {
   const [results, setResults] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const data = await GetMovies();
+        const data = await GetMovies(genresString);
         console.log(data);
         setResults(data.results); // Update the state with the fetched genres
       } catch (error) {
@@ -18,16 +18,16 @@ function MoviesByGenres() {
     }
 
     fetchData();
-  }, []);
+  }, [genresString]);
 
   return (
     <>
-      <Genres />
+      {/* <Genres /> */}
       <div className="flex justify-center">
       <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {results &&
           results.map((result) => (
-            <div className="max-w-xs rounded overflow-hidden shadow-lg">
+            <div key={result.id} className="max-w-xs rounded overflow-hidden shadow-lg">
               <img
                 className="w-full"
                 src={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
