4e59f546cfa9aeca283c607e5001980cae610120
sonia-robin <cervinkova.sonik@gmail.com>
2024-02-19 20:08:12 +0000

    Adds MovieDetails function returning movie details data

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import GetMovieDetails from "../utils/Api-details";
+
+function MovieDetails({ movieId }) {
+  // const {id} = useParams();
+  // console.log(id);
+
+  const [results, setResults] = useState({});
+
+  useEffect(() => {
+    async function fetchDetails() {
+      try {
+        const data = await GetMovieDetails(movieId);
+        console.log(data);
+        setResults(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchDetails();
+    console.log(results);
+  }, [movieId]);
+
+  return (
+    <>
+      <h2>MovieDetails</h2>
+      <p>{results.title}</p>
+      <h3>Overview</h3>
+      <p>{results.overview}</p>
+      {results.genres && (
+        <ul>
+          Genres:
+          {results.genres.map((genre) => (
+            <li key={genre.id}>{genre.name}</li>
+          ))}
+        </ul>
+      )}
+      <p>Release date: {results.release_date}</p>
+      <p>Rating: {results.vote_average}</p>
+      <img
+        src={`https://image.tmdb.org/t/p/w342/${results.poster_path}`}
+        alt="poster"
+      ></img>
+    </>
+  );
+}
+
+export default MovieDetails;
