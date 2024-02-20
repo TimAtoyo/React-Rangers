82ead4e930a0e772f31e7c2d867eda7a3445548a
sonia-robin <cervinkova.sonik@gmail.com>
2024-02-20 00:23:11 +0000

    Renders movie reviews

--- src/components/MovieDetailsResult.jsx
+++ src/components/MovieDetailsResult.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MovieDetails from "./MovieDetails";
+import MovieReviews from "./MovieReviews";
 
 function MovieDetailsResult() {
   const { id } = useParams();
-  return <MovieDetails movieId={id} />;
+  return (
+    <>
+        <MovieDetails movieId={id} />
+        <MovieReviews movieId={id} />
+    </>
+  
+  )
 }
 
 export default MovieDetailsResult;
